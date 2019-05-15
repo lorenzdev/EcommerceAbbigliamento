4f2748dea9bb802ef037c1dbafa3f3af38813109
Luca Tedeschini <luca28@outlook.com>
2019-05-15 15:57:50 +0200

    Creata struttura I/O Client-Server
    -Creata struttura che sarà la base delle
    future comunicazioni I/O tra Client e Server.
    
    -Aggiunga breve lista TODO nel file ClientThread
    
    -(BUG)La comunicazione funziona, ma se il client prova a procedere per l'autenticazione entra in un loop in cui non può uscire

--- Versione Java/javaSocketTCP/src/javasockettcp/ClientTCP.java
+++ Versione Java/javaSocketTCP/src/javasockettcp/ClientTCP.java
@@ -1,97 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javasockettcp;
 
 import java.net.*;
 import java.io.*;
 import java.util.*;
 
-/**
- *
- * @author lorenzovitale
- */
 
 
 public class ClientTCP {
     
-    private static String partenza;
-    private static String arrivo;
-    private static String orPartenza;
-    private static String orArrivo;
-    private static String costo;
-    private static String scelta;
+
     public ClientTCP(String address, int port){
-        if (scelta.equals("1")){
+        //creo lo scanner per le future operazioni di Input
+        Scanner lettura = new Scanner(System.in);
+        String cmd="";
+
             try{
-                // CREO IL SOCKET
+                //creo il socket per la comunicazione con il server
                 Socket client = new Socket(address, port);
 
-                // CREO LO STREAM SUL SOCKET
+                //creo i canali di comunicazione con il server
                 PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
-
-                // SCRIVO SULLO STREAM DEL SOCKET
-                out.println(scelta);
-                out.println(partenza);
-                out.println(arrivo);
-                System.out.println("ho inviato i dati "+arrivo+" "+partenza);
-
-                // RICEVO IL MESSAGGIO DALLO STREAM DEL CLIENT
                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                System.out.println("Il server risponde: \n");
-                String risp = in.readLine();
-                while (risp != null){
-                    System.out.println(risp);
-                    risp=in.readLine();
-                }
+                while(!cmd.equals("EXIT")){
+                    
+                    System.out.println("Avviata comunicazione con il server, digitare EXIT quando possibile per chiudere il programma \n");
 
-                client.close();
+                    //ciclo per le risposte del server
+                    String risp = in.readLine();
+                    while (risp != null && !risp.equals("Spezzano")){
+                        System.out.println(risp);
+                        risp=in.readLine();
+                    }
+                    //leggo la risposta dell'utente
+                    cmd=lettura.nextLine();
+                    //la invio al server
+                    out.println(cmd);
+
+
+                }
+            //comando per chiudere il client
+            client.close(); 
+            }
+            catch(IOException ex){
+            System.out.println("C'è stato un errore nella comunicazione con il Server!");
 
             }
-            catch(Exception ex){
-            ex.printStackTrace();
         }
-       }
-        
-        
-        else{
-            try{
-                // CREO IL SOCKET
-                Socket client = new Socket(address, port);
-                int provoadaggiungereunariga=0;
-                // CREO LO STREAM SUL SOCKET
-                PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
 
-                // SCRIVO SULLO STREAM DEL SOCKET
-                out.println(scelta);
-                out.println(partenza);
-                out.println(arrivo);
-                out.println(orPartenza);
-                out.println(orArrivo);
-                out.println(costo);
-                
-                System.out.println("ho inviato i dati "+arrivo+" "+partenza+" "+orPartenza+" "+orArrivo+" "+costo);
-
-                // RICEVO IL MESSAGGIO DALLO STREAM DEL CLIENT
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                System.out.println("Il server risponde: \n");
-                String risp = in.readLine();
-                while (risp != null){
-                    System.out.println(risp);
-                    risp=in.readLine();
-                }
-
-                client.close();
-
-            }
-            catch(Exception ex){
-            ex.printStackTrace();
-        }
-        }
-        
-    }
+    
     
     
     public static void main(String args[]){
@@ -105,48 +62,8 @@ public class ClientTCP {
         Scanner sc_port = new Scanner(System.in);
         String str_port = sc_port.nextLine();
         
-        System.out.println("Vuoi leggere (1) o scrivere (2) ?");
-        Scanner sceltaS = new Scanner(System.in);
-        scelta = sceltaS.nextLine();
-        
-        if(scelta.equals("1"))
-        {
-            System.out.println("Luogo partenza");
-            Scanner sc_service = new Scanner(System.in);
-            partenza = sc_service.nextLine();
-
-            System.out.println("Luogo Arrivo");
-            sc_service = new Scanner(System.in);
-            arrivo = sc_service.nextLine();
-
-            int port = Integer.parseInt(str_port);
-            new ClientTCP(address, port);
-        }
-        else{
-            System.out.println("Luogo partenza");
-            Scanner sc_service = new Scanner(System.in);
-            partenza = sc_service.nextLine();
-
-            System.out.println("Luogo Arrivo");
-            sc_service = new Scanner(System.in);
-            arrivo = sc_service.nextLine();
-            
-            System.out.println("Orario partenza");
-            Scanner bla = new Scanner(System.in);
-            orPartenza = bla.nextLine();
-
-            System.out.println("Orario arrivo");
-            Scanner blaa = new Scanner(System.in);
-            orArrivo = blaa.nextLine();
-            
-            System.out.println("Costo");
-            Scanner blaaa = new Scanner(System.in);
-            costo = blaaa.nextLine();
-            
-
-            int port = Integer.parseInt(str_port);
-            new ClientTCP(address, port);
-        }
+        int port = Integer.parseInt(str_port);
+        new ClientTCP(address, port);
     }
     
 }
--- Versione Java/javaSocketTCP/src/javasockettcp/ClientThread.java
+++ Versione Java/javaSocketTCP/src/javasockettcp/ClientThread.java
@@ -1,146 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package javasockettcp;
 
 import java.net.*;
 import java.io.*;
-import java.util.*;
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.*;
-import javax.xml.transform.stream.*;
-import org.w3c.dom.*;
-import org.xml.sax.*;
-import java.io.*;
 
-
-/**
- *
- * @author lorenzovitale
- */
 public class ClientThread extends Thread{
-    ArrayList<ArrayList<String>> treni;
+    //creo le variabili che utilizzeò per la comunicazione
     Socket client;
     BufferedReader in;
     PrintWriter out;
-    Document doc;
+
     
-    // COSTRUTTORE
-    public ClientThread(Socket client, ArrayList<ArrayList<String>> treni, Document doc){
+    //costruttore
+    public ClientThread(Socket client){
         this.client = client;
-        this.treni = treni;
-        this.doc = doc;
+
     }
     
-    
+    @Override
     public void run(){
-       
+        
+        //TODO comunicazione con il DB per le informazioni.
+        //TODO interfaccia per la registrazione
+        //TODO interfaccia per il login
+        //TODO interfaccia per inserire nuovi articoli
+        //TODO implementare servizio multicast
+        
         try{
+            //creo i canali per la comunicazione
             out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            
-            
-
-           
             // RICEVO IL DATO INVIATO DAL CLIENT
-            String partenza;
-            String arrivo;
-            String scelta;
-            String orPartenza;
-            String orArrivo;
-            String costo;
-            scelta = in.readLine();
-            if (scelta.equals("1"))
-            {
-                partenza = in.readLine();
-                arrivo = in.readLine();
-                String response = "";
-                ArrayList<ArrayList<String>> risposta = new ArrayList<ArrayList<String>>();
-                for (int i=0; i<treni.size(); i++){
-
-                    ArrayList<String> treno = treni.get(i);
-
-                    Boolean flag1 = false;
-                    Boolean flag2 = false;
-
-                    if (treno.get(0).equals(partenza))
-                        flag1=true;
-
-                    if (treno.get(1).equals(arrivo))
-                        flag2=true;
-
-                    if (flag1 && flag2)
-                        risposta.add(treno);
-
-                }
-
-                String stringa;
-                if (risposta.size()<= 0)
-                    out.println("nessun treno trovato");
-                else{
-                    for (int i=0;i<risposta.size();i++)
-                    {
-                        stringa = "***Treno numero "+i+"***\nPartenza="+risposta.get(i).get(0)+"\nArrivo="+risposta.get(i).get(1)+"\nOrario Partenza="+risposta.get(i).get(2)+"\nOrario Arrivo="+risposta.get(i).get(3)+"\nCosto="+risposta.get(i).get(4)+"\n\n";
-                        out.println(stringa);
-                    }
-
-                }
-            }
-            else{
-                partenza = in.readLine();
-                arrivo = in.readLine();
-                orPartenza = in.readLine();
-                orArrivo = in.readLine();
-                costo = in.readLine();
-                
-                ArrayList<String> treno0 = new ArrayList<String>();
-                treno0.add(partenza);
-                treno0.add(arrivo);
-                treno0.add(orPartenza);
-                treno0.add(orArrivo);
-                treno0.add(costo);
-                treni.add(treno0);
-                
-                String stringa;
-                
-                stringa = "***Treno aggiunto ***\nPartenza="+treno0.get(0)+"\nArrivo="+treno0.get(1)+"\nOrario Partenza="+treno0.get(2)+"\nOrario Arrivo="+treno0.get(3)+"\nCosto="+treno0.get(4)+"\n\n";
-                out.println(stringa);
-            }
+            //in.readLine();
+            //out.println();
+            //comendi per chiudere le comunicazioni
+            //in.close();
+            //out.close();
+            //client.close();
             
+            //primo messaggio che riceve il client
+            out.println("Benvenuto nel servizio di eCommerce, al momento può solo registrarsi o loggarsi\n");
+            out.println("Loggarsi (1) o Registrarsi (2) ?\n");
             
-            NodeList tmpEmployees = ((Element)doc.getFirstChild()).getElementsByTagName("treno");
-            for(int i = 0;i < tmpEmployees.getLength();i++){
-            Node treno = tmpEmployees.item(i);
-                if(treno.getNodeType() == Node.ELEMENT_NODE){
-                    Element el = (Element)treno;
-                    Boolean flag3 = false;
-                    Boolean flag4 = false;
-                    if (el.getElementsByTagName("luogoP").item(0).getTextContent().equals(partenza))
-                        flag3=true;
-                    
-                    if(el.getElementsByTagName("luogoA").item(0).getTextContent().equals(arrivo))
-                        flag4=true;
-                    out.println("+++ TRENO TROVATO +++\nPartenza="+el.getElementsByTagName("luogoP").item(0).getTextContent()+"\nArrivo="+el.getElementsByTagName("luogoA").item(0).getTextContent()+"\nOrario Partenza="+el.getElementsByTagName("orarioP").item(0).getTextContent()+"\nOrario Arrivo="+el.getElementsByTagName("orarioA").item(0).getTextContent()+"\nCosto="+el.getElementsByTagName("costo").item(0).getTextContent());
-
-                
-                }
-            }
-            
-            
-            
-            // CHIUDO I BUFFER E IL SOCKET
-            in.close();
-            out.close();
-            client.close();
-            
-        }catch(Exception e){
-            e.printStackTrace();
+            //stringa che fa uscire dall'ascolto il client e permette di rispondere
+            out.println("Spezzano");
+        }catch(IOException e){
+            out.println("C'è stato un errore nel server, riprova!");
         }
-        
-        
     }
-    
 }
