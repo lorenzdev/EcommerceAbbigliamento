54968a2d884508b1d492dc3dc7b0a032ba9516f3
Luca <luca.teddy28@gmail.com>
2019-05-21 12:08:16 +0200

    bvasdds

--- Versione Java/javaSocketTCP/src/javasockettcp/ClientThread.java
+++ Versione Java/javaSocketTCP/src/javasockettcp/ClientThread.java
@@ -47,91 +47,91 @@ public class ClientThread extends Thread{
             //out.close();
             //client.close();
             
-            //primo messaggio che riceve il client
-            out.println("Benvenuto nel servizio di eCommerce, al momento può solo registrarsi o loggarsi\n");
-            out.println("Loggarsi (1) o Registrarsi (2) ?\n");
             
-            //stringa che fa uscire dall'ascolto il client e permette di rispondere
-            out.println("Spezzano");
+            do{
+                //primo messaggio che riceve il client
+                out.println("Benvenuto nel servizio di eCommerce, al momento può solo registrarsi o loggarsi\n");
+                out.println("Loggarsi (1) o Registrarsi (2) ?\n");
 
- 
-            //TODO una specie di chiave di sessione, che una volta loggato ti identifichi
-            
-            String pw;
-            String email;
+                //stringa che fa uscire dall'ascolto il client e permette di rispondere
+                out.println("Spezzano");
 
-            risp=in.readLine();
-                try{
-            switch (Integer.parseInt(risp))
-                    {
-                case 1:
-                    out.println("Inserisci l'email");
-                    out.println("Spezzano");
-                    email=in.readLine();
-                    out.println("Inserisci la password");
-                    out.println("Spezzano");
-                    pw=in.readLine();
-                    String risposta=FunzioniServer.Login(email, pw);
-                    out.println(risposta);
-                    //si potrebbe ricavare il nome, ma al momento va bene così
-                    if (risposta.equals("Loggato correttamente!\n"))
-                        nomeClient=email;
 
-                    break;
+                //TODO una specie di chiave di sessione, che una volta loggato ti identifichi
 
+                String pw;
+                String email;
+
+                risp=in.readLine();
+                    try{
+                switch (Integer.parseInt(risp))
+                        {
+                    case 1:
+                        out.println("Inserisci l'email");
+                        out.println("Spezzano");
+                        email=in.readLine();
+                        out.println("Inserisci la password");
+                        out.println("Spezzano");
+                        pw=in.readLine();
+                        String risposta=FunzioniServer.Login(email, pw);
+                        out.println(risposta);
+                        //si potrebbe ricavare il nome, ma al momento va bene così
+                        if (risposta.equals("Loggato correttamente!\n"))
+                            nomeClient=email;
+
+                        break;
+
+
+                    //probabilmente sarebbe più carino farlo con un vettore e un loop
+                    case 2:
+                        String nome;
+                        String cognome;
+                        String numeroTelefono;
+                        String indirizzo;
+                        String dataNascita;
+                        String citta;
+                        out.println("Inserisci l'e-mail");
+                        out.println("Spezzano");
+                        email=in.readLine();
+                        out.println("Inserisci il nome");
+                        out.println("Spezzano");
+                        nome=in.readLine(); 
+                        out.println("Inserisci il cognome");
+                        out.println("Spezzano");
+                        cognome=in.readLine();
+                        out.println("Inserisci la password");
+                        out.println("Spezzano");
+                        pw=in.readLine();
+                        out.println("Inserisci il numero di telefono");
+                        out.println("Spezzano");
+                        numeroTelefono=in.readLine();
+                        out.println("Inserisci indirizzo");
+                        out.println("Spezzano");
+                        indirizzo=in.readLine();
+                        out.println("Inserisci la data di nascita");
+                        out.println("Spezzano");
+                        dataNascita=in.readLine();
+                        out.println("Inserisci la citta");
+                        out.println("Spezzano");
+                        citta=in.readLine();
+                        out.println(FunzioniServer.Registrarsi(email,nome, cognome,pw,numeroTelefono,indirizzo,dataNascita,citta));
+
+                        //out.println("Spezzano");
+                        break;
+                    default:
+                        out.println("inserisci un valore valido (1) o (2)");
+                        break;
+                }                    
+                }catch(Exception e)
+                {
+                    out.println("inserisci un valore valido");
+
+                }
+
+                //se non si è loggato
+
+            } while(nomeClient.equals(">>>>----<<<<"));
                     
-                //probabilmente sarebbe più carino farlo con un vettore e un loop
-                case 2:
-                    String nome;
-                    String cognome;
-                    String numeroTelefono;
-                    String indirizzo;
-                    String dataNascita;
-                    String citta;
-                    out.println("Inserisci l'e-mail");
-                    out.println("Spezzano");
-                    email=in.readLine();
-                    out.println("Inserisci il nome");
-                    out.println("Spezzano");
-                    nome=in.readLine(); 
-                    out.println("Inserisci il cognome");
-                    out.println("Spezzano");
-                    cognome=in.readLine();
-                    out.println("Inserisci la password");
-                    out.println("Spezzano");
-                    pw=in.readLine();
-                    out.println("Inserisci il numero di telefono");
-                    out.println("Spezzano");
-                    numeroTelefono=in.readLine();
-                    out.println("Inserisci indirizzo");
-                    out.println("Spezzano");
-                    indirizzo=in.readLine();
-                    out.println("Inserisci la data di nascita");
-                    out.println("Spezzano");
-                    dataNascita=in.readLine();
-                    out.println("Inserisci la citta");
-                    out.println("Spezzano");
-                    citta=in.readLine();
-                    out.println(FunzioniServer.Registrarsi(email,nome, cognome,pw,numeroTelefono,indirizzo,dataNascita,citta));
-                    
-                    //out.println("Spezzano");
-                    break;
-                default:
-                    out.println("inserisci un valore valido (1) o (2)");
-                    break;
-            }                    
-            }catch(Exception e)
-            {
-                out.println("inserisci un valore valido");
-
-            }
-                
-            //se non si è loggato
-            if (nomeClient.equals(">>>>----<<<<"))
-            {
-                //fa ripartire il thread da capo, ma è una soluzione valida?
-                this.run();
-            }
             
             
             out.println("Benvenuto "+nomeClient+", ora che è loggato può vedere la lista dei prodotti(1) oppure aggiungerne uno lei(2)");
@@ -156,7 +156,7 @@ public class ClientThread extends Thread{
                     break;
 
                 case 2:
-                    //aggiungerne uno
+                    
                     break;
                     
                 default:
