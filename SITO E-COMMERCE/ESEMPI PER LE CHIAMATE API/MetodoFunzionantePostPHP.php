c0a8839c28a0ba477aab8a57339c7b1fdb645e28
Luca Tedeschini <luca28@outlook.com>
2019-05-29 18:32:01 +0200

    Creato file di esempio per le chiamate API

--- SITO E-COMMERCE/ESEMPI PER LE CHIAMATE API/MetodoFunzionantePostPHP.php
+++ SITO E-COMMERCE/ESEMPI PER LE CHIAMATE API/MetodoFunzionantePostPHP.php
@@ -5,18 +5,26 @@
 <meta charset="UTF-8" />
 </head>
 <body>
+
+
 <?php
+//tutta sta roba ti conviene farla in una funzione che chiede in ingresso la variabile $data e l'url, e poi returna l'$header
+//costruire un array con i dati che richiede il sito (vedi LEGGIMI.txt)
 $data=array("email" => "prova",
 			"password" => "provaa");
-
+//questo va lasciato uguale
 $opts = array('http'=>array(
 				'header' => "Content-type: application/x-www-form-urlencoded\r\n",
 				'method'  => 'POST',
 				'content' => json_encode($data)));
 $context = stream_context_create($opts);
-$header = file_get_contents('http://lucatedeschini2000.pythonanywhere.com/API/Login',false,$context);
-echo $header;
+//url dell'api
+$url='http://lucatedeschini2000.pythonanywhere.com/API/Login';
+//con sto comando invii i dati e riceve la risposta nella variabile
+$header = file_get_contents($url,false,$context);
+//traduce il risultato in un vettore
 $header = json_decode($header, true);
+//per vedere i nomi del vettore guarda il file LEGGIMI.txt
 echo $header['status'];
 ?>
 
